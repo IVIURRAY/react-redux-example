70c07ee7b75aaabdc016611dab875cfde20a7d7b
IVIURRAY <haydn.murray1@yahoo.co.uk>
2020-01-20 21:06:00 +0000

    refeactor

--- src/sagas/api-saga.js
+++ src/sagas/api-saga.js
@@ -1,4 +1,5 @@
 import { takeEvery, call, put } from "redux-saga/effects";
+import { DATA_LOADED } from "../constants/action-types";
 
 export default function* watcherSaga() {
     yield takeEvery("DATA_REQUESTED", workerSaga);
@@ -7,7 +8,7 @@ export default function* watcherSaga() {
 function* workerSaga() {
     try {
         const payload = yield call(getData);
-        yield put({ type: "DATA_LOADED", payload });
+        yield put({ type: DATA_LOADED, payload });
     } catch (e) {
         yield put({ type: "API_ERRORED", payload: e });
     }
